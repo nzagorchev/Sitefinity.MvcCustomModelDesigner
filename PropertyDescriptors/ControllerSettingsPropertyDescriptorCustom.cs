37e3f531c3677a2e82fcf702afc82b691d7b8f4d
nzagorchev <nikola.zagorchev@gmail.com>
2016-05-25 15:06:31 +0300

    Added fix for Preview functionality
    When in Preview, the descriptors flow is different. Added a fix to
    resolve the control data resolution in Preview.

--- PropertyDescriptors/ControllerSettingsPropertyDescriptorCustom.cs
+++ PropertyDescriptors/ControllerSettingsPropertyDescriptorCustom.cs
@@ -1,18 +1,20 @@
 ﻿using SitefinityWebApp.PropertyDescriptors.Converters;
-using System;
-using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
 using System.Reflection;
-using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+using Telerik.Microsoft.Practices.Unity;
 using Telerik.Sitefinity.Abstractions;
+using Telerik.Sitefinity.Mvc;
+using Telerik.Sitefinity.Mvc.Proxy;
 using Telerik.Sitefinity.Mvc.Proxy.TypeDescription;
 using Telerik.Sitefinity.Pages;
-using Telerik.Microsoft.Practices.Unity;
+using Telerik.Sitefinity.Pages.Model;
 
 namespace SitefinityWebApp.PropertyDescriptors
 {
-    public class ControllerSettingsPropertyDescriptorCustom : ControllerSettingsPropertyDescriptor
+    public class ControllerSettingsPropertyDescriptorCustom : ControllerSettingsPropertyDescriptor, IControlPropertyDescriptor
     {
         public ControllerSettingsPropertyDescriptorCustom(PropertyInfo propertyInfo)
             : base(propertyInfo)
@@ -26,6 +28,46 @@ namespace SitefinityWebApp.PropertyDescriptors
             {
                 return new ControllerSettingsTypeConverterCustom();
             }
+        }
+
+        public virtual new PropertyDescriptorCollection GetChildProperties(ControlProperty data)
+        {
+            PropertyDescriptorCollection properties = null;
+            var controllerNameProperty = data.Control.Properties.Where(p => p.Name == "ControllerName").FirstOrDefault();
+            if (controllerNameProperty != null)
+            {
+                Controller controllerInstanceFromName = this.GetControllerInstanceFromName(controllerNameProperty.Value);
+                ControllerSettings controllerSettings = new ControllerSettings(controllerInstanceFromName);
+
+                properties = base.GetChildProperties(data); // TypeDescriptor.GetProperties(controllerSettings);
+                // get the properties for the component via instance
+                for (int i = 0; i < properties.Count; i++)
+                {
+                    PropertyDescriptor prop = properties[i];
+                    if (controllerSettings.Values.ContainsKey(prop.Name))
+                    {
+                        var actualProp = controllerSettings.Values[prop.Name];
+                        if (prop.PropertyType.FullName != actualProp.GetType().FullName)
+                        {
+                            prop = new DynamicPropertyDescriptorPreview(prop.Name, controllerSettings);
+                            properties.RemoveAt(i);
+                            properties.Add(prop);
+                        }
+                    }
+
+                }
+            }
+
+            return properties;
+        }
+
+        private Controller GetControllerInstanceFromName(string controllerName)
+        {
+            if (string.IsNullOrEmpty(controllerName))
+            {
+                return null;
+            }
+            return (Controller)ObjectFactory.Resolve<ISitefinityControllerFactory>().CreateController(new RequestContext(), controllerName);
         }
 
         public static void Install(string controlPropertyDescriptorName)
---
+++ PropertyDescriptors/DynamicPropertyDescriptorPreview.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Telerik.Sitefinity.Mvc.Proxy;
+
+namespace SitefinityWebApp.PropertyDescriptors
+{
+    public class DynamicPropertyDescriptorPreview : DynamicPropertyDescriptorCustom
+    {
+        public DynamicPropertyDescriptorPreview(string propertyName, IControllerSettings instance)
+            : base(propertyName, instance)
+        {
+        }
+
+        public override Type PropertyType
+        {
+            get
+            {
+                if (this.instance.Values.ContainsKey(this.propertyName))
+                {
+                    if (this.instance.Values[this.propertyName] != null)
+                    {
+                        return this.instance.Values[this.propertyName].GetType();
+                    }
+                }
+
+                return base.PropertyType;
+            }
+        }
+    }
+}
